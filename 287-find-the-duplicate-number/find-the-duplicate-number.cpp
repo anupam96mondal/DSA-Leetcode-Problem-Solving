311e534981b4d44ab6c03f5d3a949cd57ef5d98f
anupam96mondal <anupam17it@gmail.com>
2025-10-08 16:06:24 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (69.39%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int number=-1;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i] == nums[i+1]){
-                number = nums[i];
-                return nums[i];
+        // sort(nums.begin(),nums.end());
+        // int number=-1;
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(nums[i] == nums[i+1]){
+        //         number = nums[i];
+        //         return nums[i];
+        //     }
+        // }
+        // return number;
+        for(int i=0;i<nums.size();i++){
+            int ind = abs(nums[i]);
+            if(nums[ind]<0){ // if the index is alredy negative that means  this is the duplicate element
+                return ind;
             }
+            nums[ind]= -nums[ind]; // negate all the elemnt in the array 
         }
-        return number;
+        return -1;
+
+
     }
 };
