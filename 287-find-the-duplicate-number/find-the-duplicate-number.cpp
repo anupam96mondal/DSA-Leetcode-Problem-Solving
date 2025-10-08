945157fc85757d5c8aecb0740a05127d0c57f39d
anupam96mondal <anupam17it@gmail.com>
2025-10-08 16:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 65.2 MB (27.95%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        // Solution 1
+
         // sort(nums.begin(),nums.end());
         // int number=-1;
         // int n = nums.size();
@@ -11,15 +13,35 @@ public:
         //     }
         // }
         // return number;
-        for(int i=0;i<nums.size();i++){
-            int ind = abs(nums[i]);
-            if(nums[ind]<0){ // if the index is alredy negative that means  this is the duplicate element
-                return ind;
-            }
-            nums[ind]= -nums[ind]; // negate all the elemnt in the array 
+
+        // Solution 2
+
+        // for(int i=0;i<nums.size();i++){
+        //     int ind = abs(nums[i]);
+        //     if(nums[ind]<0){ // if the index is alredy negative that means  this is the duplicate element
+        //         return ind;
+        //     }
+        //     nums[ind]= -nums[ind]; // negate all the elemnt in the array 
+        // }
+        // return -1;
+
+        // Solution 3  Hare and Tortoise
+
+        int slow= nums[0];
+        int fast = nums[0];
+
+        do{
+            slow =nums[slow];
+            fast = nums[nums[fast]];
         }
-        return -1;
+        while(slow!=fast);
 
+        fast = nums[0];
 
+        while(slow!=fast){
+            slow= nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
     }
 };
