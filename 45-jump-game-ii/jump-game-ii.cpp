6a2c36cb3c4022d9fd89f6cdc94b4d91e4a127e7
anupam96mondal <anupam17it@gmail.com>
2025-10-09 12:00:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (37.09%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-       int jump =0;
-       int n = nums.size();
-       int l=0;
-       int r =0;
+        int n = nums.size();
+        int jump =0;
+        int l=0;
+        int r =0;
 
-        while(r< n-1){
-            int farthest =0;
+        while(r<n-1){
+            int farthest =0; // find furthest jump in range index
             for(int index = l; index<=r;index++){ // range search of a max length jump
                 farthest = max(index+nums[index], farthest);
             }
-            l =r+1;
-            r= farthest;
-            jump=jump+1;
+            l =r+1; // update l pointer next to r
+            r = farthest; // update r pointer to farthest
+            jump = jump+1; // increase jump count for next iteration
         }
         return jump;
     }
